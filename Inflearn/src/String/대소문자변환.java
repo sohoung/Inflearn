46a3badca8c56e7eca024330c619c532c7b68e5e
김민성 <sohoung4@gmail.com>
2023-01-18 16:54:43 +0900

    no message

--- Inflearn/src/String/대소문자변환.java
+++ Inflearn/src/String/대소문자변환.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 // 설명 : 대문자와 소문자가 같이 있는 문자열을 입력받아 대문자는 소문자로, 소문자는 대문자로 변환하는 프로그램을 작성하세요
 // 입력 예시 : StuDY
 // 출력 예시 : sTUdy
+// 향상된 for문으로 코딩 가능
 public class 대소문자변환 {
     public static void main(String[] args) {
         대소문자변환 m = new 대소문자변환();
         Scanner sc = new Scanner(System.in);
         String str = sc.next();  // 입력받을 문자열
         String answer = "";  // 출력값을 위한 String형 answer
-        for(int i = 0; i < str.length(); i++) {  // for문으로 조건 검색
+        for(int i = 0; i < str.length(); i++) {  // for문으로 조건 검사
             if(Character.isLowerCase(str.charAt(i))){  // 소문자이면 대문자로
                 answer += Character.toUpperCase(str.charAt(i));
             }
