26794ab0476b4b71df2d52bab5c8b79831aee353
김민성 <sohoung4@gmail.com>
2023-01-18 16:54:48 +0900

    no message

--- Inflearn/src/String/문장속단어.java
+++ Inflearn/src/String/문장속단어.java
@@ -1,10 +1,14 @@
 package String;
 import java.util.Scanner;
 
-// 설명 :
+// 설명 : 한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력하는 프로그램을 작성하세요.
+//       문장속의 각 단어는 공백으로 구분됩니다.
+// 입력 예시 : it is time to study
+// 출력 예시 : study
 public class 문장속단어 {
     public static void main(String[] args) {
         문장속단어  m = new 문장속단어();
         Scanner sc = new Scanner(System.in);
+
     }
 }
