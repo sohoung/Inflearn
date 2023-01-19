f7d4e43662024b114845c8d44489dd33b6ef6882
김민성 <sohoung4@gmail.com>
2023-01-19 14:53:01 +0900

    숫자만 추출

---
+++ Inflearn/src/String/숫자만추출.java
@@ -0,0 +1,14 @@
+package String;
+import java.util.Scanner;
+
+// 설명 : 문자와 숫자가 섞여있는 문자열이 주어지면 그 중 숫자만 추출하여 그 순서대로 자연수를 만듭니다.
+//       만약 “tge0a1h205er”에서 숫자만 추출하면 0, 1, 2, 0, 5이고 이것을 자연수를 만들면 1205이 됩니다.
+//       추출하여 만들어지는 자연수는 100,000,000을 넘지 않습니다.
+// 입력 예시 : g0en2T0s8eSoft
+// 출력 에시 : 208
+public class 숫자만추출 {
+    public static void main(String[] args) {
+        숫자만추출 t = new 숫자만추출();
+        Scanner sc = new Scanner(System.in);
+    }
+}
