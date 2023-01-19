1cb4b83b8d4073361f4a35bd9bfa1711b0899295
김민성 <sohoung4@gmail.com>
2023-01-19 10:45:56 +0900

    no message

---
+++ Inflearn/src/String/특정문자뒤집기.java
@@ -0,0 +1,20 @@
+package String;
+import java.util.Scanner;
+
+// 설명 : 영어 알파벳과 특수문자로 되어있는 문자열이 주어지고 영어 알파벳만 뒤집고 나머지는 그 자리 그대로 출력하는 프로그램을 작성하세요.
+// 입력 예시 : a#b!GE*T@S
+// 출력 예시 : S#T!EG*b@a
+public class 특정문자뒤집기 {
+    public String solution(String str,char[] c) {
+        String answer = "";
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        특정문자뒤집기 t = new 특정문자뒤집기();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] c = str.toCharArray();
+        
+    }
+}
