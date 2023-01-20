33934fa57ac5fa88fb20c8f61b0590c1c90e4cb1
김민성 <sohoung4@gmail.com>
2023-01-20 21:31:21 +0900

    문자거리

--- Inflearn/src/String/문자거리.java
+++ Inflearn/src/String/문자거리.java
@@ -1,7 +1,23 @@
 package String;
 
-public class 문자거리 {
-    public static void main(String[] args) {
+// 설명 : 한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램을 작성하세요.
+// 입력 예시 : teachermode e
+// 출력 예시 : 1 0 1 2 1 0 1 2 2 1 0
 
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class 문자거리 {
+    public ArrayList<Integer> solution(String str){
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        문자거리 t = new 문자거리();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();  // 입력받을 문자
+        String ch = sc.next();   // 거리를 구해야 하는 문자
+        
     }
 }
