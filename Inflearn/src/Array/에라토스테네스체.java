d4380815973d45fac693b1425649e737d5e8e28d
김민성 <sohoung4@gmail.com>
2023-01-24 00:15:52 +0900

    에라토스테네스 체

--- Inflearn/src/Array/에라토스테네스체.java
+++ Inflearn/src/Array/에라토스테네스체.java
@@ -13,7 +13,7 @@ public class 에라토스테네스체 {
         for (int i = 2; i <= n; i++) {
             if (arr[i] == 0) {
                 answer++;
-                for (int j = i; j <= n; j += i) arr[j] = 1;  
+                for (int j = i; j <= n; j += i) arr[j] = 1;
             }
         }
         return answer;
