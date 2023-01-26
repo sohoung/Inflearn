4b4def747e7dbcddb49a48b30a004574e189e55e
김민성 <sohoung4@gmail.com>
2023-01-26 15:58:07 +0900

    멘토링

--- Inflearn/src/Array/멘토링.java
+++ Inflearn/src/Array/멘토링.java
@@ -12,22 +12,44 @@ import java.util.Scanner;
 //           3 1 4 2
 // 출력 예시 : 3
 public class 멘토링 {
-    public int solution(int n, int[][] arr) {
+    public int solution(int n,  int m, int[][] arr) {
         int answer = 0;
-        
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                int cnt = 0;
+                for(int k = 0; k < m; k++) {
+                    int pi = 0;
+                    int pj = 0;
+                    for(int s = 0; s < n; s++) {
+                        if(arr[k][s] == i) {
+                            pi = s;
+                        }
+                        if(arr[k][s] == j) {
+                            pj = s;
+                        }
+                    }
+                    if(pi < pj) {
+                        cnt++;
+                    }
+                }
+                if(cnt == m) {
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
     public static void main(String[] args) {
         멘토링 t = new 멘토링();
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        int n = sc.nextInt();  // 학생 수
+        int m = sc.nextInt();  // 테스트 갯수
         int[][] arr = new int[m][n];
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
-        System.out.println(t.solution(n,arr));
+        System.out.println(t.solution(n,m,arr));
     }
 }
