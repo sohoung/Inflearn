b2a011aab4f3cb6a0697354ae6ef5cf7537229ee
김민성 <sohoung4@gmail.com>
2023-01-27 11:08:16 +0900

    2개의 배열 합치기

--- Inflearn/src/TwoPointers_SlidingWindows/두배열합치기.java
+++ Inflearn/src/TwoPointers_SlidingWindows/두배열합치기.java
@@ -1,6 +1,5 @@
 package TwoPointers_SlidingWindows;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 // 설명 : 오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램을 작성하세요.
@@ -12,19 +11,34 @@ import java.util.Scanner;
 public class 두배열합치기 {
     public ArrayList<Integer> solution(int n, int m, int[] arr1, int[] arr2) {
         ArrayList<Integer> answer = new ArrayList<>();
-        
+        int p1 = 0;  // arr1의 포인터
+        int p2 = 0;  // arr2의 포인터
+        while(p1 < n && p2 < m) {
+            if(arr1[p1] < arr2[p2]) {  // arr1이 arr2보다 작을 때
+                answer.add(arr1[p1++]);
+            }
+            else {                     // arr1이 arr2보다 큰 경우와 arr1이 arr2와 같은 경우
+                answer.add(arr2[p2++]);
+            }
+        }
+        while(p1 < n) {  // arr1의 데이터가 남았을 경우
+            answer.add(arr1[p1++]);
+        }
+        while(p2 < m) {  // arr2의 데이터가 남았을 경우
+            answer.add(arr2[p2++]);
+        }
         return answer;
     }
     public static void main(String[] args) {
         두배열합치기 t = new 두배열합치기();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr1 = new int[n];
+        int[] arr1 = new int[n];  // 첫 번째 배열
         for(int i = 0; i < n; i++) {
             arr1[i] = sc.nextInt();
         }
         int m = sc.nextInt();
-        int[] arr2 = new int[m];
+        int[] arr2 = new int[m];  // 두 번째 배열
         for(int i = 0; i < m; i++) {
             arr2[i] = sc.nextInt();
         }
